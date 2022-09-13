6e5a2d3360d2e61f8d3da10791282141afa3a911
manijs82 <Manijavdani1@gmail.com>
2022-09-13 19:21:48 +0430

    Day4

---
+++ Assets/Scripts/Day4/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Scripts.Day4
+{
+    public class Ball : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            AddForce(15);
+        }
+
+        private void AddForce(float force)
+        {
+            _rigidbody.AddForce(Vector3.forward * force, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Day4/DestroyOnImpact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Scripts.Day4
+{
+    public class DestroyOnImpact : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Bottom")) 
+                Destroy(gameObject);
+        }
+    }
+}
